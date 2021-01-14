6fe8280edf47b6c3d06d8a41d097b76b66070c38
Danilo <dprosa23@gmail.com>
2021-01-13 23:26:26 -0300

    Projeto inicial

---
+++ front-web/src/Orders/StepsHeader.tsx
@@ -0,0 +1,28 @@
+function StepsHeader() {
+    return (
+        <header className="orders-steps-container">
+            <div className="orders-steps-content">
+                <div>
+                <h1 className="steps-title">
+                    SIGA AS <br /> ETAPAS
+                </h1>
+                </div> 
+                <div>
+                <ul className="steps-items">
+                    <li>
+                        <span className="steps-number">1</span>
+                        Selecione os produtos e localização
+                    </li>
+                    <li>
+                        <span className="steps-number">2</span>
+                        Depois clique em <strong>ENVIAR PEDIDO</strong>
+                    </li>
+                    
+                </ul>
+                </div>   
+            </div>
+        </header>
+    )
+}
+
+export default StepsHeader;
